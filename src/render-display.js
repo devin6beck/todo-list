ad6a40fd9a485fa95e089eb5fba9fd81a6d240d3
Devin Beck <devin6beck@gmail.com>
2022-03-29 17:19:48 -0600

    add button for new task

--- src/render-display.js
+++ src/render-display.js
@@ -3,4 +3,12 @@ export default function renderDisplay(project) {
   const displayHeader = document.querySelector('.display-header');
   display.textContent = project.title;
   displayHeader.textContent = project.title;
+  display.appendChild(createTaskBtn());
+}
+
+function createTaskBtn() {
+  const taskBtn = document.createElement('button');
+  taskBtn.classList.add('btn-task');
+  taskBtn.textContent = '+';
+  return taskBtn;
 }
